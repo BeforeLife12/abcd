5449149c1ba8ab25c19234ae77f5556bc9b12cfc
BeforeLife12 <128486243+BeforeLife12@users.noreply.github.com>
2024-03-21 19:46:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+status = "";
+objects = [];
+alarm = "alarm.wav";
+
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(380 , 380);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(380,380);
+video.hide();
+
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+console.log("Model Loaded!");
+status=true;
+
+}
+
+function draw(){
+image(video , 0 , 0 , 380 , 380);
+if(status != "")
+{
+    r = random(255);
+    g = random(255);
+    b = random(255);
+     objectDetector.detect(video , gotResult);
+    for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML = "Status : Objects Detected ! ";
+        document.getElementById("baby_found").innerHTML = "baby found ";
+        fill(r , g , b);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x , objects[i].y );
+        noFill();
+        stroke(r , g , b);
+        rect(objects[i].x , objects[i].y ,objects[i].width , objects[i].height);
+
+    }
+}
+else {
+    document.getElementById("baby_found").innerHTML = "baby not found ";
+    alarm.play();
+
+}
+if(objects < 0){
+    document.getElementById("baby_found").innerHTML = "baby not found ";
+    alarm.play();
+}
+
+
+
+
+
+}
+
+function gotResult(error , results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+       
+    }
+}
+
